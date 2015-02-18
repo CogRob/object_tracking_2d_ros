520f4eaa6f2bb91d60d80b43edfcdeffee47f855
ruffsl <roxfoxpox@gmail.com>
2015-02-18 17:20:46 -0500

    Commenting more code and removing gloabal vars

--- include/object_tracking_2d_ros.h
+++ include/object_tracking_2d_ros.h
@@ -1,9 +1,16 @@
+
+// Include the trackers from the object_tracking_2d project
 #include "include/object_tracking_2D/tracker_irls.h"
 #include "include/object_tracking_2D/tracker_pf_texture.h"
 #include "include/object_tracking_2D/tracker_pf_textureless.h"
 
-#include "ObjectDetection.h"
 
+// Include object_tracking_2d_ros message types
+#include "ObjectDetection.h"
+#include <object_tracking_2d_ros/ObjectDetections.h>
+
+
+// Define the default topic names
 const std::string DEFAULT_IMAGE_TOPIC = "image";
 const std::string DEFAULT_CAMERA_INFO_TOPIC = "camera_info";
 const std::string DEFAULT_MARKER_TOPIC = "marker_array";
@@ -14,12 +21,13 @@ ros::NodeHandlePtr node_;
 boost::shared_ptr<image_transport::ImageTransport> image_;
 sensor_msgs::CameraInfo camera_info_;
 
+// ROS Publishers and Subscribers
 ros::Publisher marker_publisher_;
 ros::Publisher ebt_publisher_;
 ros::Subscriber info_subscriber;
 image_transport::Subscriber image_subscriber;
 
-// EBT parts
+// EBT plugin perams
 std::string ebt_tracker_type_;
 std::string ebt_mesh_resource_;
 int ebt_num_particle_;
@@ -38,25 +46,40 @@ bool ebt_display_;
 int ebt_th_canny_l_;
 int ebt_th_canny_h_;
 
-CvMat* pose_init_ = cvCreateMat(4, 4, CV_32F);
-CvMat* pose_cv_;
-Eigen::Matrix4d pose_;
-
-TrackerBase* tracker_;
-
 // Settings and local information
 bool viewer_;
 bool running_;
 bool has_camera_info_;
 std::string display_type_;
 
+// Package Functions
+
+// Returns pose from detection
 Eigen::Matrix4d GetDetectionTransform(ObjectDetection detection);
+
+// Callback for camera info
 void InfoCallback(const sensor_msgs::CameraInfoConstPtr& camera_info);
-void InfoCallback(const sensor_msgs::CameraInfoConstPtr& camera_info);
+
+// Callback for new subcription
 void ConnectCallback(const ros::SingleSubscriberPublisher& info);
+
+// Callback for unsubcription
 void DisconnectCallback(const ros::SingleSubscriberPublisher& info);
+
+// Handler for unsubcription
 void DisconnectHandler();
+
+// Read Parameters for unsubcription
 void GetParameterValues();
+
+// Advertize Publishers
 void SetupPublisher();
+
+// Initialize EBT tracker using Parameters
 void InitializeTracker();
+
+// Initialize the ROS Node
 void InitializeROSNode();
+
+// Global EBT tracker
+TrackerBase* tracker_;
--- src/object_tracking_2d_ros.cpp
+++ src/object_tracking_2d_ros.cpp
@@ -1,52 +1,53 @@
+//Include ROS libraries for node messages
 #include <ros/ros.h>
 #include <ros/forwards.h>
 #include <ros/single_subscriber_publisher.h>
 #include <sensor_msgs/Image.h>
 #include <image_transport/image_transport.h>
-#include <visualization_msgs/Marker.h>
+
+// Include OpenCV for images and viewer
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <cv_bridge/cv_bridge.h>
 
+//Include Eigen tools for pose handeling
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+//Include Visualization tools for mesh marker
+#include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
-#include "yaml-cpp/yaml.h"
-#include <sstream>
-#include <fstream>
 
+//Include Boost tools for detection arrays
 #include <boost/unordered_set.hpp>
 #include <boost/unordered_map.hpp>
 #include <boost/make_shared.hpp>
 
+//Include the base of object_tracking_2d_ros
 #include "object_tracking_2d_ros.h"
-#include <object_tracking_2d_ros/ObjectDetections.h>
 
 
 using namespace std;
 
-// Functions
-
 Eigen::Matrix4d GetDetectionTransform(ObjectDetection detection)
 {
+    // Draft function for now
     return detection.pose;
 }
 
-// Callback for camera info
 void InfoCallback(const sensor_msgs::CameraInfoConstPtr& camera_info)
 {
     camera_info_ = (*camera_info);
     has_camera_info_ = true;
 }
 
-// Callback for image data
 void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-//    if(!has_camera_info_){
-//        ROS_WARN("No Camera Info Received Yet");
-//        return;
-//    }
+    // Dont atempt to use the image without having info about the camera first
+    if(!has_camera_info_){
+        ROS_WARN("No Camera Info Received Yet");
+        return;
+    }
 
     // Get the image
     cv_bridge::CvImagePtr subscribed_ptr;
@@ -59,28 +60,34 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-    
     cv::Mat subscribed_gray = subscribed_ptr->image;
     cv::Mat tmp;
     cv::Point2d opticalCenter(0.5*subscribed_gray.rows,
                               0.5*subscribed_gray.cols);
-    ObjectDetectionArray detections;
 
+    // Apply the tracker to the image
     tracker_->setImage(subscribed_gray);
     tracker_->tracking();
-    ObjectDetection d;
 
+    // Convert pose matrix from CvMat to Eigen
+    CvMat* pose_cv_;
+    Eigen::Matrix4d pose_;
     pose_cv_ = tracker_->obj_model_->pose_;
     for(int r = 0; r < 4; r++){
         for(int c = 0; c < 4; c++){
             pose_(r,c) = CV_MAT_ELEM(*pose_cv_, float, r, c);
         }
     }
+
+    // Store the detection into an array struture
+    ObjectDetection d;
     d.pose = pose_;
     d.good = true;
     d.id = 1;
+    ObjectDetectionArray detections;
     detections.push_back(d);
 
+    // Store the detection into an array struture
     visualization_msgs::MarkerArray marker_transforms;
     object_tracking_2d_ros::ObjectDetections object_detections;
     object_detections.header.frame_id = msg->header.frame_id;
@@ -88,11 +95,13 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
 
     if(viewer_)
     {
+        // Render the results for the viewer
         CvScalar color = cvScalar(255,255,0);
         tracker_->obj_model_->displayPoseLine(tracker_->img_result_, pose_cv_, color, 1, false);
         tracker_->obj_model_->displaySamplePointsAndErrors(tracker_->img_edge_);
     }
 
+    // Loop over each detection
     for(unsigned int i = 0; i < detections.size(); ++i)
     {
         // skip bad detections
@@ -100,13 +109,13 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
         {
             continue;
         }
-        
+
+        // Get quaternion for the marker
         Eigen::Matrix4d pose = GetDetectionTransform(detections[i]);
-        
-        // Get this info from earlier code, don't extract it again
         Eigen::Matrix3d R = pose.block<3,3>(0,0);
         Eigen::Quaternion<double> q(R);
-        
+
+        // Set the attributes for the marker
         visualization_msgs::Marker marker_transform;
         marker_transform.header.frame_id = msg->header.frame_id;
         marker_transform.header.stamp = msg->header.stamp;
@@ -115,12 +124,18 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
         marker_transform.ns = convert.str().c_str();
         marker_transform.id = detections[i].id;
 
+        // Set the object mesh for the marker
         marker_transform.type = visualization_msgs::Marker::MESH_RESOURCE;
         marker_transform.mesh_resource = ebt_mesh_path_;
         marker_transform.scale.x = 1;
         marker_transform.scale.y = 1;
         marker_transform.scale.z = 1;
+        marker_transform.color.r = 0.0;
+        marker_transform.color.g = 1.0;
+        marker_transform.color.b = 0.0;
+        marker_transform.color.a = 1.0;
 
+        // Set the marker's pose
         marker_transform.action = visualization_msgs::Marker::ADD;
         marker_transform.pose.position.x = pose(0,3);
         marker_transform.pose.position.y = pose(1,3);
@@ -129,29 +144,29 @@ void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
         marker_transform.pose.orientation.y = q.y();
         marker_transform.pose.orientation.z = q.z();
         marker_transform.pose.orientation.w = q.w();
-        
-        marker_transform.color.r = 0.0;
-        marker_transform.color.g = 1.0;
-        marker_transform.color.b = 0.0;
-        marker_transform.color.a = 1.0;
+
+        // Add the marker to marker array message
         marker_transforms.markers.push_back(marker_transform);
-        
+
         // Fill in Object detection.
         object_tracking_2d_ros::ObjectDetection object_det;
         object_det.header = marker_transform.header;
         object_det.id = marker_transform.id;
         object_det.pose = marker_transform.pose;
 
+        // Add the detection to detection array message
         object_detections.detections.push_back(object_det);
     }
+
+    // Publish the marker and detection messages
     marker_publisher_.publish(marker_transforms);
     ebt_publisher_.publish(object_detections);
-    
+
     if(viewer_)
     {
+        // Show the results for the viewer
         cv::Mat my_image = tracker_->img_edge_;
         cv::imshow("ObjectTrackin2D", my_image);
-//        cv::imshow("ObjectTrackin2D", subscribed_gray);
     }
 }
 
@@ -159,7 +174,7 @@ void ConnectCallback(const ros::SingleSubscriberPublisher& info)
 {
     // Check for subscribers.
     uint32_t subscribers = marker_publisher_.getNumSubscribers()
-                           + ebt_publisher_.getNumSubscribers();
+            + ebt_publisher_.getNumSubscribers();
     ROS_DEBUG("Subscription detected! (%d subscribers)", subscribers);
 
     if(subscribers && !running_)
@@ -167,14 +182,14 @@ void ConnectCallback(const ros::SingleSubscriberPublisher& info)
         ROS_DEBUG("New Subscribers, Connecting to Input Image Topic.");
         ros::TransportHints ros_transport_hints(ros::TransportHints().tcpNoDelay());
         image_transport::TransportHints image_transport_hint(image_transport::TransportHints(
-                                "raw", ros_transport_hints, (*node_),
-                                "image_transport"));
+                                                                 "raw", ros_transport_hints, (*node_),
+                                                                 "image_transport"));
 
         image_subscriber = (*image_).subscribe(
-                DEFAULT_IMAGE_TOPIC, 1, &ImageCallback,
-                image_transport_hint);
+                    DEFAULT_IMAGE_TOPIC, 1, &ImageCallback,
+                    image_transport_hint);
         info_subscriber = (*node_).subscribe(
-                DEFAULT_CAMERA_INFO_TOPIC, 10, &InfoCallback);
+                    DEFAULT_CAMERA_INFO_TOPIC, 10, &InfoCallback);
         running_ = true;
     }
 }
@@ -187,9 +202,9 @@ void DisconnectCallback(const ros::SingleSubscriberPublisher& info)
 {
     // Check for subscribers.
     uint32_t subscribers = marker_publisher_.getNumSubscribers()
-                           + ebt_publisher_.getNumSubscribers();
+            + ebt_publisher_.getNumSubscribers();
     ROS_DEBUG("Unsubscription detected! (%d subscribers)", subscribers);
-    
+
     if(!subscribers && running_)
     {
         ROS_DEBUG("No Subscribers, Disconnecting from Input Image Topic.");
@@ -223,20 +238,22 @@ void GetParameterValues()
 }
 
 void SetupPublisher()
-{    
+{
+    // Add callbacks
     ros::SubscriberStatusCallback connect_callback = &ConnectCallback;
     ros::SubscriberStatusCallback disconnect_callback = &DisconnectCallback;
-    
+
     // Publisher
     marker_publisher_ = node_->advertise<visualization_msgs::MarkerArray>(
-            DEFAULT_MARKER_TOPIC, 1, connect_callback,
-            disconnect_callback);
+                DEFAULT_MARKER_TOPIC, 1, connect_callback,
+                disconnect_callback);
     ebt_publisher_ = node_->advertise<object_tracking_2d_ros::ObjectDetections>(
-            DEFAULT_DETECTIONS_TOPIC, 1, connect_callback, disconnect_callback);
+                DEFAULT_DETECTIONS_TOPIC, 1, connect_callback, disconnect_callback);
 }
 
 void InitializeTracker()
 {
+    // Create the desired tracker
     if(ebt_tracker_type_.compare("irls") == 0)
     {
         tracker_ = new IrlsTracker ();
@@ -260,6 +277,7 @@ void InitializeTracker()
         ROS_ERROR_STREAM("Unknown tracker method name: " << ebt_tracker_type_);
     }
 
+    // Configure the desired tracker
     tracker_->setSampleStep(ebt_sample_step_);
     tracker_->setDisplay(ebt_display_);
     tracker_->setNetworkMode(false);
@@ -269,9 +287,12 @@ void InitializeTracker()
     std::string ach_channel = "none";
 
     // oeverwrite pose_init
-    for (int i = 0; i < 16; i++)
+    CvMat* pose_init_ = cvCreateMat(4, 4, CV_32F);
+    for (int i = 0; i < 16; i++){
         pose_init_->data.fl[i] = ebt_init_pose_[i];
+    }
 
+    // Initialize the desired tracker
     tracker_->initTracker(ebt_obj_path_, input, ebt_intrinsic_, ebt_distortion_, ebt_width_, ebt_height_, pose_init_ , ach_channel);
 }
 
@@ -284,16 +305,19 @@ void InitializeROSNode(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
+    // Initialize Node
     InitializeROSNode(argc,argv);
     GetParameterValues();
     SetupPublisher();
     InitializeTracker();
 
     if(viewer_){
+        // Make a cv window for the viewer
         cvNamedWindow("ObjectTrackin2D");
         cvStartWindowThread();
     }
 
+    // Start Node
     ROS_INFO("ObjectTrackin2D node started.");
     running_ = false;
     has_camera_info_ = false;

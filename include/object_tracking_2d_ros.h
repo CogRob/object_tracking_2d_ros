a36dec3635a0327c8cc07276c4350804d502d7bd
ruffsl <roxfoxpox@gmail.com>
2015-04-17 00:08:24 -0400

    Adding user event input with key input thread

--- include/object_tracking_2d_ros.h
+++ include/object_tracking_2d_ros.h
@@ -3,6 +3,7 @@
 #include <ros/forwards.h>
 #include <ros/single_subscriber_publisher.h>
 #include <sensor_msgs/Image.h>
+#include <std_msgs/String.h>
 #include <image_transport/image_transport.h>
 #include <dynamic_reconfigure/server.h>
 
@@ -16,11 +17,12 @@
 #include <cv_bridge/cv_bridge.h>
 
 //Include Visualization tools for mesh marker
-#include <visualization_msgs/Marker.h>
+//#include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <interactive_markers/interactive_marker_server.h>
 
-//Include Boost tools for detection arrays
+//Include Boost tools for detection arrays and threads
+#include <boost/thread.hpp>
 #include <boost/unordered_set.hpp>
 #include <boost/unordered_map.hpp>
 #include <boost/make_shared.hpp>
@@ -35,6 +37,9 @@
 #include <object_tracking_2d_ros/ObjectDetections.h>
 #include <object_tracking_2d_ros/object_tracking_2d_rosConfig.h>
 
+// Include user input tools
+#include "user_input_thread.h"
+
 // Define the default topic names
 const std::string DEFAULT_IMAGE_TOPIC       = "image";
 const std::string DEFAULT_CAMERA_INFO_TOPIC = "camera_info";
@@ -43,6 +48,7 @@ const std::string DEFAULT_DETECTIONS_TOPIC  = "detections";
 const std::string DEFAULT_IMAGE_RESULT_TOPIC= "image_result";
 const std::string DEFAULT_IMAGE_EDGE_TOPIC  = "image_edge";
 const std::string DEFAULT_INIT_POSES_TOPIC  = "init_poses";
+const std::string DEFAULT_USER_INPUT_TOPIC  = "remote_key";
 
 // ROS parts
 ros::NodeHandlePtr node_;
@@ -56,6 +62,7 @@ ros::Publisher img_result_publisher_;
 ros::Publisher img_edge_publisher_;
 ros::Subscriber info_subscriber;
 ros::Subscriber init_poses_subscriber;
+ros::Subscriber sub_remote_key_;
 image_transport::Subscriber image_subscriber;
 
 // Config
@@ -67,6 +74,7 @@ std::string ebt_tracker_type_;
 std::string ebt_mesh_resource_;
 int ebt_num_particle_;
 int ebt_min_keypoint_;
+bool ebt_init_;
 double ebt_th_cm_;
 std::string ebt_obj_path_;
 std::string ebt_mesh_path_;
@@ -89,7 +97,11 @@ bool viewer_;
 bool viewing_;
 bool running_;
 bool has_camera_info_;
+bool user_input_;
+bool quit_;
 std::string display_type_;
+std::string remote_input_;
+boost::shared_ptr<vk::UserInputThread> user_input_thread_;
 
 // Package Functions
 
